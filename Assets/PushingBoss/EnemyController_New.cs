a7f14ee6fb5e1c2d7dda1329d20574fa9924326a
Clod-ai <58184572+Clod-ai@users.noreply.github.com>
2022-07-20 03:20:38 -0500

    Testing boss

---
+++ Assets/PushingBoss/EnemyController_New.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController_New : MonoBehaviour
+{
+
+    public float lookRadius = 30f;
+
+    Transform target;
+    NavMeshAgent agent;
+    public Animator mAnimator;
+    private NavMeshAgent mNavMeshAgent;
+    private int ataque = 0;
+
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        mNavMeshAgent = GetComponent<NavMeshAgent>();
+    }
+    void Start()
+    {
+        target = PlayerManager.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if(distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+            mNavMeshAgent.isStopped = false;
+            mNavMeshAgent.destination = target.position;
+            mAnimator.SetTrigger("Walk");
+
+            if (distance <= agent.stoppingDistance)
+            {
+                //attack
+                FaceTarget();
+            }
+            
+        }else
+        {
+                mAnimator.SetTrigger("Stop");
+                mNavMeshAgent.isStopped = true;
+        }
+
+        if(distance <= 3f)
+        {
+            mAnimator.SetTrigger("Melee");
+            ataque++;
+        }
+        if(ataque != 0)
+        {
+            mAnimator.SetTrigger("MeleeOut");
+            ataque = 0;
+        }
+    }
+
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position, lookRadius);
+    }
+}
